75aae0ed33242c488ce684ed6c98cf9de095fafc
richikarangta <richika.rangta@gmail.com>
2018-10-05 20:45:41 +0100

    name 10,20...90

--- number_words/lib/naming_numbers.rb
+++ number_words/lib/naming_numbers.rb
@@ -1,7 +1,7 @@
 #Naming Numbers
 
 def name_numbers( num )
- name = {
+ name_ones = {
   	1 => 'one',
   	2 => 'two',
   	3 => 'three',
@@ -10,9 +10,24 @@ def name_numbers( num )
   	6 => 'six',
   	7 => 'seven',
   	8 => 'eight',
-  	9 => 'nine',
-  	10 => 'ten',
+  	9 => 'nine', 
+  } 
+  
+  name_tens = {
+    10 => 'ten',
+  	20 => 'twenty',
+  	30 => 'thirty',
+  	40 => 'fourty',
+  	50 => 'fifty',
+  	60 => 'sixty',
+  	70 => 'seventy',
+  	80 => 'eighty',
+  	90 => 'ninety',
+  	 }
 
-  }  
-  return name.fetch(num) 
+  if num <10
+    return name_ones.fetch(num) 
+  else
+    return name_tens.fetch(num) 
+  end
 end
--- number_words/spec/naming_numbers_spec.rb
+++ number_words/spec/naming_numbers_spec.rb
@@ -6,18 +6,22 @@ RSpec.describe "naming numbers" do
   end
 
   it "name '2' as two" do
-    expect( name_numbers 1 ).to eq("one")
+    expect( name_numbers 2 ).to eq("two")
   end
 
   it "name '7' as seven" do
-    expect( name_numbers 1 ).to eq("one")
+    expect( name_numbers 7 ).to eq("seven")
   end
 
   it "name '9' as nine" do
-    expect( name_numbers 1 ).to eq("one")
+    expect( name_numbers 9 ).to eq("nine")
   end
 
   it "name '10' as ten" do
-    expect( name_numbers 1 ).to eq("one")
+    expect( name_numbers 10 ).to eq("ten")
+  end
+
+   it "name '30' as thirty" do
+    expect( name_numbers 30 ).to eq("thirty")
   end
 end  
